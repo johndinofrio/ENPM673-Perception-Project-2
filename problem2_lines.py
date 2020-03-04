6b46c2521fc44ea726a1bb03a031150f4dcdb580
alejocbs <cbueno@umd.edu>
2020-03-04 01:20:32 -0500

    Added Sobelx, sobely and laplace to test the best. Also corrected the point for the homograph. The perfect range for homograph are between pictures 215 and 230. A quick input will let you change the image. ( uncomment line 13)

--- problem2_lines.py
+++ problem2_lines.py
@@ -2,38 +2,53 @@
 
 import numpy as np
 import cv2 as cv
+import glob
 from matplotlib import pyplot as plt
 import helpers
 
 #get the image
-img = cv.imread('./Videos/data_1/0000000000.png')
+img =[cv.imread(File) for File in glob.glob("./Videos/data_1/data/*.png")]
+img2 = np.array(img)
 
-
-while (1):
+#num = int(input("Enter the number of the picture from 0-302: ")) # uncomment this line to use a different image
+num=230 # best image
+while True:
     #show the original image
-    cv.imshow("rezied src", img)
+    file = img2[num]
+    # 4 points on the original image's lane lines (two from each line)
+    source = np.array([[0,432], [280,350], [763,350], [820,432]])
+    # 4 new points line) for where the image should line up
+    dest = np.array([[294.0, 463.0], [294.0, 341.0], [892.0, 341.0], [892.0, 463.0]])
+    cv.line(file, (0,432), (280,350), (0,255,0), 2)
+    cv.line(file, (763,350), (820,432), (0, 255, 0), 2)
+    cv.imshow("rezied src", file)
 
-    #4 points on the original image's lane lines (two from each line) 
-    source = np.array([[294.0,463.0],[510.0,331.0],[770.0,327.0],[892.0,472.0]])
-    #4 new points line) for where the image should line up
-    dest = np.array([[294.0,463.0],[294.0,341.0],[892.0,341.0],[892.0,463.0]])
-    #find homography H from the given 8 points 
-    h,status = cv.findHomography(source,dest)
-    #create the top down view
-    unwarped = cv.warpPerspective(img, h, (1000,500))
+    # find homography H from the given 8 points
+    h, status = cv.findHomography(source, dest)
+    # create the top down view
+    #newimg = cv.resize(file,(1000,500))
+    unwarped = cv.warpPerspective(file, h, (1000, 500))
     cv.imshow("unwarped", unwarped)
 
-    #threshold the image in binary
-    unwarped_gray = cv.cvtColor(unwarped,cv.COLOR_BGR2GRAY)
-    ret,BW_lanes = cv.threshold(unwarped_gray,200,255,cv.THRESH_BINARY)
+    # threshold the image in binary
+    unwarped_gray = cv.cvtColor(unwarped, cv.COLOR_BGR2GRAY)
+    ret, BW_lanes = cv.threshold(unwarped_gray, 200, 255, cv.THRESH_BINARY)
     cv.imshow("BW Lanes", BW_lanes)
-    #perform Canny edge detection
-    edges = cv.Canny(BW_lanes,50,100,apertureSize = 3)
-    
-    #quit program if user presses escape or 'q'
+    # perform Canny edge detection
+    edges = cv.Canny(BW_lanes, 50, 100, apertureSize=3)
+    laplacian = cv.Laplacian(BW_lanes, cv.CV_64F)
+    sobelx = cv.Sobel(BW_lanes, cv.CV_64F, 1, 0, ksize=5)  # x
+    sobely = cv.Sobel(BW_lanes, cv.CV_64F, 0, 1, ksize=5)  # y
+    cv.imshow("Canny", edges)
+    cv.imshow("laplacian", laplacian)
+    cv.imshow("Sobelx", sobelx)
+    cv.imshow("Sobely", sobely)
+    # quit program if user presses escape or 'q'
+
     key = cv.waitKey() & 0xFF
     if key == 27 or key == ord("q"):
         break
+
     
 
 cv.destroyAllWindows()
